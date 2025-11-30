39426cc52d7bf3a25938386ede99fb690b6e79f8
mohamed1fares <110127407+mohamed1fares@users.noreply.github.com>
2025-11-30 18:17:53 +0200

    Update user info

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,8 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { User } from '../models/users.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+
+  constructor(private _http:HttpClient){}
+
+  url = environment.apiUrl +'users';
+
+  getAllUsers():Observable<User[]>{
+    return this._http.get<User[]>(this.url + '/')
+  }
+
+
+
+  editUserStatus(id:string,payload:{userActive?: 'active' | 'notActive'; userPending?: 'pending' | 'accepted';role?: 'admin' | 'user'}): Observable<User>{
+    return this._http.put<{message?:string; data:User}>(`${this.url}/edit/status/${id}`,payload).pipe(
+      map(res=>res.data ?? (res as any)),
+      catchError(err=>{
+        console.error('Failed to update user status',err);
+        return throwError(()=>err);
+      })
+    );
+  }
+  
+  
+
   
 }
--- src/app/dashboard/pages/users-info/users-info.ts
+++ src/app/dashboard/pages/users-info/users-info.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserService } from '../../../core/services/user.service';
+import { User } from '../../../core/models/users.model';
 
 @Component({
   selector: 'app-users-info',
@@ -8,26 +10,128 @@ import { CommonModule } from '@angular/common';
   templateUrl: './users-info.html',
   styles: []
 })
-export class UsersInfo {
-  approvedUsers = [
-    { id: 1, name: 'Alice Brown', email: 'alice@example.com', role: 'Admin', status: 'Active', lastActive: '5 min ago' },
-    { id: 2, name: 'Bob Wilson', email: 'bob@example.com', role: 'Security Analyst', status: 'Active', lastActive: '1 hour ago' },
-    { id: 3, name: 'Charlie Taylor', email: 'charlie@example.com', role: 'Developer', status: 'Active', lastActive: '3 hours ago' },
-    { id: 4, name: 'Diana Martinez', email: 'diana@example.com', role: 'Penetration Tester', status: 'Active', lastActive: '1 day ago' }
-  ];
+export class UsersInfo implements OnInit {
+constructor(private _userService:UserService){}
+allUsersCount: number = 0;
+users :User[]=[]
 
-  edit(id: number) {
-    const user = this.approvedUsers.find(u => u.id === id);
-    alert(`✏️ Editing user: ${user?.name}`);
+
+  // approvedUsers = [
+  //   { id: 1, name: 'Alice Brown', email: 'alice@example.com', role: 'Admin', status: 'Active', lastActive: '5 min ago' },
+  //   { id: 2, name: 'Bob Wilson', email: 'bob@example.com', role: 'Security Analyst', status: 'Active', lastActive: '1 hour ago' },
+  //   { id: 3, name: 'Charlie Taylor', email: 'charlie@example.com', role: 'Developer', status: 'Active', lastActive: '3 hours ago' },
+  //   { id: 4, name: 'Diana Martinez', email: 'diana@example.com', role: 'Penetration Tester', status: 'Active', lastActive: '1 day ago' }
+  // ];
+
+  // edit(id?: string) {
+  //   const user = this.users.find(u => u._id === id);
+  //   alert(`✏️ Editing user: ${user?.fristName } ${user?.lastName}`);
+  // }
+
+  // delete(id?: string) {
+  //   const user = this.users.find(u => u._id === id);
+  //   if (user) {
+  //       if (confirm(`Delete user ${user.fristName}?`)) {
+  //           // this.users = this.users.filter(u => u._id !== id);
+  //           this._userService.editUserStatus(id!, { userActive: 'notActive' }).subscribe({
+  //               next: (res) => {
+  //                   console.log('User deleted:', res);
+  //                   this.users = this.users.filter(u => u._id !== id);
+  //               },
+  //               error: (err) => console.error('Error deleting user:', err)
+  //           })
+  //           alert(`🗑️ User ${user.fristName} deleted.`);
+  //       }
+  //   }
+  // }
+
+  delete(userId?: string) {
+    if (!userId) return alert('User id missing');
+    if (confirm(`you are sure to stop ${this.users.find(u => u._id === userId)?.fristName} account's ?` )) {
+    this._userService.editUserStatus(userId, { userActive: 'notActive', userPending: 'accepted' })
+      .subscribe({
+        next: updated => {
+          // alert(`User updated: ${updated.email}`);
+          this.ngOnInit(); 
+        },
+        error: err => {
+          console.error(err);
+          alert('Failed to update user');
+        }
+      });
+    }
   }
 
-  delete(id: number) {
-    const user = this.approvedUsers.find(u => u.id === id);
-    if (user) {
-        if (confirm(`Delete user ${user.name}?`)) {
-            this.approvedUsers = this.approvedUsers.filter(u => u.id !== id);
-            alert(`🗑️ User ${user.name} deleted.`);
+  restore(userId?: string) {
+    if (!userId) return alert('User id missing');
+    if (confirm(`you are sure to restore ${this.users.find(u => u._id === userId)?.fristName} account's ?` )) {
+    this._userService.editUserStatus(userId, { userActive: 'active' })
+      .subscribe({
+        next: updated => {
+          // alert(`User updated: ${updated.email}`);
+          this.ngOnInit(); 
+        },
+        error: err => {
+          console.error(err);
+          alert('Failed to update user');
         }
+      });
     }
+  }
+
+
+  toUser(userId?: string) {
+    if (!userId) return alert('User id missing');
+    if (confirm(`you are sure to change ${this.users.find(u => u._id === userId)?.fristName} account's to be a user?` )) {
+    this._userService.editUserStatus(userId, { role: 'user' })
+      .subscribe({
+        next: updated => {
+          // alert(`User updated: ${updated.email}`);
+          this.ngOnInit(); 
+        },
+        error: err => {
+          console.error(err);
+          alert('Failed to update user');
+        }
+      });
+    }
+  }
+  
+
+
+  toAdmin(userId?: string) {
+    if (!userId) return alert('User id missing');
+    if (confirm(`you are sure to change ${this.users.find(u => u._id === userId)?.fristName} account's to be an admin?` )) {
+    this._userService.editUserStatus(userId, { role: 'admin' })
+      .subscribe({
+        next: updated => {
+          // alert(`User updated: ${updated.email}`);
+          this.ngOnInit(); 
+        },
+        error: err => {
+          console.error(err);
+          alert('Failed to update user');
+        }
+      });
+    }
+  }
+
+
+  ngOnInit(): void {
+
+    this._userService.getAllUsers().subscribe({
+      next:(res)=>{
+        this.users=res;
+
+        this.allUsersCount=res.length;
+        console.log(this.users);
+        
+        console.log('All users:',res);
+      },
+        error:(err)=>console.error('Error fetching users:',err)
+    }
+  );
+
+    
   }
 }
