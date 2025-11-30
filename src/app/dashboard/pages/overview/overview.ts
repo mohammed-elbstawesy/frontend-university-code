6481ee3e41bfd5efa727b4d57f37ae52dc574f84
mohammed-elbstawesy <mohammed3mto25mm@gmail.com>
2025-11-30 23:21:14 +0200

    update dashboard quick

--- src/app/dashboard/pages/overview/overview.ts
+++ src/app/dashboard/pages/overview/overview.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuickActions } from '../../quick-actions/quick-actions';
+// import { QuickActions } from '../../quick-actions/quick-actions';
 import { Stats } from '../../stats/stats';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [CommonModule, Stats, QuickActions],
+  imports: [CommonModule, Stats],
   templateUrl: './overview.html',
   styles: []
 })
